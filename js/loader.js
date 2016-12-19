2064d490a71f1434b8b7c991a2ae820a2fd264a4
abhishekg785 <abhishekg785@gmail.com>
2016-12-19 21:28:32 +0530

    animation added

---
+++ js/loader.js
@@ -0,0 +1,13 @@
+/**
+ *  @author : abhishek goswami
+ *  @github : abhishekg785
+ *  @gmail : abhishekg785@gmail.com
+ *
+ *  loader.js : logic for implementing the loader
+ */
+
+(function($, d, w) {
+    $.fn.loader = function() {
+        console.log('in the loader function');
+    }
+})(jQuery, document, window);
---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ *  @author : abhishek goswami (hiro)
+ *  @github : abhishekg785
+ *
+ *  main.js
+ */
+
+(function($, d, w) {
+})(jQuery, document, window);
