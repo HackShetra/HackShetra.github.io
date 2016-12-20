2e2adb92f1ee6a1fcd29a8d5422f7c015886fc94
abhishekg785 <abhishekg785@gmail.com>
2016-12-20 23:21:52 +0530

    indentation added

--- js/title/title.js
+++ js/title/title.js
@@ -2,16 +2,16 @@
     $(d).ready(function() {
         $('.titleWrapper').addClass('ready');
         $('.titleWrapper h1').each(function() {
-           var fullString;
-           var characters = $(this).text().split("");
-           $this = $(this);
-           $this.empty();
-           $.each(characters, function(i, el) {
-              if(el == "") {
-                  el = "&nbsp;"
-              }
-              $this.append("<span>" + el + "</span>");
-           });
+            var fullString;
+            var characters = $(this).text().split("");
+            $this = $(this);
+            $this.empty();
+            $.each(characters, function(i, el) {
+                if(el == "") {
+                    el = "&nbsp;"
+                }
+                $this.append("<span>" + el + "</span>");
+            });
         });
     });
 })(jQuery, window, document);
