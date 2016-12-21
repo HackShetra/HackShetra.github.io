3be629e0f31e60d912b02cadd7b40f50bc9b1cac
abhishekg785 <abhishekg785@gmail.com>
2016-12-21 23:45:57 +0530

    typing effect created

---
+++ js/background/typing_effect.js
@@ -0,0 +1,66 @@
+/**
+ *  @author : abhishek goswami ( hiro )
+ *  @github : abhishekg785
+ *
+ *  typing_effect.js : creates a typewriter effect
+ */
+
+(function($, w, d){
+
+    function CreateTypingEffect(element, toRotateTextArr, period) {
+        this.element = element;
+        this.toRotateTextArr = toRotateTextArr;
+        this.period = parseInt(period, 10) || 2000;
+        this.loopNum = 0;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    }
+
+    CreateTypingEffect.prototype.tick = function() {
+        var i = this.loopNum % this.toRotateTextArr.length;
+        var fullText = this.toRotateTextArr[i];
+
+        if(this.isDeleting) {
+            this.txt = fullText.substring(0, this.txt.length - 1);
+            console.log(this.txt);
+        }
+        else {
+            this.txt = fullText.substring(0, this.txt.length + 1);
+            console.log(this.txt);
+        }
+        this.element.innerHTML = '<span class = "typing-text">'+ this.txt + '</span>';
+        var that = this;
+        var delta = 300 - Math.random() * 100;
+        if(this.isDeleting) {
+            delta /= 2;
+        }
+
+        if(!this.isDeleting && this.txt === fullText) {
+            delta = this.period;
+            this.isDeleting = true;
+        }
+        else if(this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    }
+
+    $(w).on('load', function() {
+        var textRotate = $('.txt-rotate');
+        for(var i = 0 ; i < textRotate.length; i++) {
+            var toRotateText = textRotate[i].getAttribute('data-rotate');
+            var period = textRotate[i].getAttribute('data-period');
+            console.log(toRotateText);
+            if(toRotateText) {
+                new CreateTypingEffect(textRotate[i], JSON.parse(toRotateText), period);
+            }
+        }
+    });
+
+})(jQuery, window, document);
